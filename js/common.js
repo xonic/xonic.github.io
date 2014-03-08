26a249ac531b338ecb00c849786848bad597a9c6
Thomas Naegele <xonic@gmx.eu>
2014-03-08 17:44:18 +0100

    - add relative path plugin - modify plugin for posts - consolidate section--alt for all pages - add section--alt to articles page

--- js/common.js
+++ js/common.js
@@ -1,34 +1,4 @@
 
-//The build will inline common dependencies into this file.
-requirejs.config(
-{
-	paths:
-	{
-		// Libraries
-		'jquery'					: '../js/vendor/jquery-1.10.2',
-		'modernizr'					: '../js/vendor/modernizr',
-		'selectivizr'				: '../js/vendor/customized/selectivizr',
-
-		// Custom modules
-		'slider'					: '../js/modules/Slider'
-	},
-
-	shim:
-	{
-		// Since it's not require-jquery.js
-		// we need to shim it.
-		'jquery':
-		{
-			exports: '$'
-		},
-
-		'modernizr':
-		{
-			exports: 'Modernizr'
-		}
-	}
-});
-
 require(["slider", "modernizr", "selectivizr"], function(Slider, Modernizr, Selectivizr)
 {
 	// Add this event listener to prevent mobile safari 
