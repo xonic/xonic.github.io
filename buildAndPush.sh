e104bf2b020e37efeedf10f34f692864c469bb64
Thomas Naegele <xonic@gmx.eu>
2014-03-08 19:55:35 +0100

    - fix script

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,6 +1,1 @@
-git checkout master
-cp -r _site/* . && rm -rf _site/ && touch .nojekyll
-git add .
-git commit -m '- precompile for github'
-git push --all origin
-git checkout source
+git checkout master && cp -r _site/* . && rm -rf _site/ && touch .nojekyll && git add . && git commit -m '- precompile for github' && git push --all origin && git checkout source
