145e76d7cd9c2a49aec3b1949157ea571e346c7a
Thomas Naegele <xonic@gmx.eu>
2014-03-10 07:20:36 +0100

    - add deobfuscator

--- js/common.js
+++ js/common.js
@@ -5,6 +5,10 @@ require(["slider", "modernizr", "selectivizr"], function(Slider, Modernizr, Sele
 	// from disabling the ::active pseudo class
 	document.addEventListener("touchstart", function() {});
 
+	// Deobfuscate email address
+	var links = $(".js-deobfuscate");
+	links.attr("href", links.attr("href").split("(at)").join("@"));
+
 	// Check if we need Selectivizr
 	if (!Modernizr.mq('only all'))
 	{
