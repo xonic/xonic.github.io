1b7b6efa89e70530d25f3790809fd5656efe7b53
Thomas Nägele <tom@naegele.it>
2017-11-22 14:12:00 +0100

    Update 2017-11-22T13:11:39.520Z

---
+++ buildAndPush.sh
@@ -0,0 +1,1 @@
+jekyll build && git checkout master && cp -r _site/* . && rm -rf _site/ && touch .nojekyll && git add . && git commit -m '- precompile for github' && git push --all origin && git checkout source && jekyll build
